df4a0123ba43eb17dcbb9f45497a3d64cce52e50
= <mikaiyl.davis@icloud.com>
2019-03-18 14:55:16 -0400

    [DONE] Fleshed out models and added some template material

--- twitter/notify/models.py
+++ twitter/notify/models.py
@@ -4,4 +4,18 @@ from django.db import models
 
 
 class Notify(models.Model):
-    pass
+    ACTION_CHOICES = (
+        ('F', 'New Follower'),
+        ('L', 'Like'),
+        ('R', 'New Reply'),
+    )
+
+    from_user = models.ForeignKey(
+        'user.TwitterUser', null=False,
+        related_name='from_user', on_delete=models.CASCADE)
+    notify_user = models.ForeignKey(
+        'user.TwitterUser', null=False,
+        related_name='to_ser', on_delete=models.CASCADE)
+    notification_type = models.CharField(max_length=1, choices=ACTION_CHOICES)
+    ref_tweet = models.ForeignKey(
+        'tweet.Tweet', null=True, on_delete=models.CASCADE)
--- twitter/tweet/models.py
+++ twitter/tweet/models.py
@@ -4,4 +4,14 @@ from django.db import models
 
 
 class Tweet(models.Model):
-    pass
+    user = models.ForeignKey(
+        'user.TwitterUser', null=False,
+        related_name='tweet_by_user', on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    body = models.TextField(max_length=280)
+    liked = models.ManyToManyField(
+        'user.TwitterUser', blank=True,
+        related_name='liked_by')
+    reply = models.BooleanField(default=False)
+    thread = models.ForeignKey(
+        "self", blank=True, null=True, on_delete=models.SET_NULL)
